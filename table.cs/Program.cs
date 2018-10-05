98d3c45f36a1a93203376323b8d2b36094f62326
Dokku <Meghana.Dokku@thomsonreuters.com>
2018-10-05 18:31:39 +0530

    Add project files.

---
+++ table.cs/Program.cs
@@ -0,0 +1,20 @@
+﻿ using System;
+
+namespace table
+{
+    class Print
+    {
+        static void Main(string[] args)
+        {
+            int x, num;
+            Console.WriteLine("enter a number");
+            num = Convert.ToInt32(Console.ReadLine());
+            for (x = 1; x <= 10; x++)
+            {
+                Console.WriteLine(x*num);
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
